0331b3dd3f396b20d18e81c41299b9951d4ff54c
Albert Guo <guozhaonan@gmail.com>
2017-04-02 16:25:17 -0400

    Initial Commit

---
+++ app/models/conversation.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+class Conversation
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :uid, type: String
+  field :context, type: Hash
+  has_many :messages
+end
---
+++ app/models/message.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+class Message
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :body, type: String
+  field :conversation_id, type: Integer
+  field :kind, type: String
+  has_many :quick_replies
+  belongs_to :conversation
+  validates_inclusion_of :kind, in: %w(outgoing incoming), allow_nil: false
+  validates :conversation, presence: true
+end
